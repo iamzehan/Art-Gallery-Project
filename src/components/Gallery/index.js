4af9ff772d9841dd6e43478af1594d6b42261233
Zehan <zehan.kareem@gmail.com>
2025-10-25 23:37:37 +0600

    refactor!(iamzehan): Gallery is handled with class

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,8 +1,23 @@
-import './style.css';
-import template from './index.html';
-const Gallery = document.createElement("div");
-Gallery.classList.add("gallery");
-
-Gallery.innerHTML = template;
-console.log(Gallery);
-export default Gallery;
+import "./style.css";
+import template from "./index.html";
+import Details from "../Details";
+export class Gallery {
+  constructor(router) {
+    this.router = router;
+    this.gallery = document.createElement("div");
+    this.gallery.classList.add("gallery");
+    this.gallery.innerHTML = template;
+    this.setupGalleryEvents();
+  }
+  getGallery() {
+    return this.gallery;
+  }
+  setupGalleryEvents() {
+    const pictures = this.gallery.querySelectorAll("div.img");
+    pictures.forEach((picture, index) => {
+      picture.addEventListener("click", () => {
+        this.router.navigate(`/gallery/details/${index + 1}`);
+      });
+    });
+  }
+}
