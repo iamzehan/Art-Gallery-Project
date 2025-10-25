31d2ebfe74377b0c76d7f5f854ab1e82f744622a
Zehan <zehan.kareem@gmail.com>
2025-10-26 03:28:33 +0600

    deploy(iamzehan): Deployment commit

--- src/components/Details/index.js
+++
@@ -1,6 +0,0 @@
-import './style.css';
-import template from './index.html';
-const Details = document.createElement("div");
-Details.classList.add("details");
-Details.innerHTML = template;
-export default Details;
